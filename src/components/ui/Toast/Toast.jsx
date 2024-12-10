fd53a0855b2e9104f10c2a38f5a2366aa53b4c8e
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2024-12-10 15:33:26 -0500

    add mobile option for toast

--- src/components/ui/Toast/Toast.jsx
+++ src/components/ui/Toast/Toast.jsx
@@ -30,29 +30,41 @@ export default function Toast({
     }, 1000);
   }, []);
 
+  const handleResize = () => {
+    const screenSize = window.innerWidth;
+    setMobile(screenSize < 800);
+  };
+
   useEffect(() => {
-    const handleResize=() => {
-      const screenSize = window.innerWidth;
-    }
-  },[])
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log(mobile);
 
   return (
-        <div className={`wrapper ${showMsg ? "" : "gone"}`}>
-          <div className={`toastWrapper ${showMsg ? "show" : ""}`}>
-            <div className={`toast notice`}>
-              <div className="iconContainer">{iconMapping[icon]}</div>
-              <p className="content">{children}</p>
-              <ButtonIcon
-                className="closeButton"
-                onClick={() => setShowMsg(false)}
-                transparent={true}
-                icon="cancel"
-                variant="black"
-                size="small"
-                aria-label="Dismiss Toast Message"
-              />
-            </div>
-          </div>
+    <div
+      className={`wrapper ${showMsg ? "" : "gone"}${mobile ? "mobile" : ""}`}
+    >
+      <div className={`toastWrapper ${showMsg ? "show" : ""}`}>
+        <div className={`toast notice`}>
+          <div className="iconContainer">{iconMapping[icon]}</div>
+          <p className="content">{children}</p>
+          <ButtonIcon
+            className="closeButton"
+            onClick={() => setShowMsg(false)}
+            transparent={true}
+            icon="cancel"
+            variant="black"
+            size="small"
+            aria-label="Dismiss Toast Message"
+          />
         </div>
+      </div>
+    </div>
   );
 }
